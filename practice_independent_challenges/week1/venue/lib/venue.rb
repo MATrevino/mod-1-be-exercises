5e719d45bdb50c2d4681472001a430ba6d111fd6
Meredith Trevino <meredithtrevino3@gmail.com>
2023-05-25 13:00:11 -0400

    adds all working tests

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,7 +1,18 @@
 class Venue
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :patrons
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(name)
+    @patrons << name
+  end
+
+  def yell_at_patrons
+    @patrons.map do |name|
+      name.upcase
+    end
   end
 end
